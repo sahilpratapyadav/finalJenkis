d0fcc802366c448ffbdf19749ff6a76b2274f656
sahilpratapyadav <sahilpratap123yadav@gmail.com>
2022-07-10 22:38:52 +0530

    first commit

---
+++ src/main/java/ashok/it/IrctApppApplication.java
@@ -0,0 +1,13 @@
+package ashok.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrctApppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrctApppApplication.class, args);
+	}
+
+}
